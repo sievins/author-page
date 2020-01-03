2648b33dd5c132bacdc0d882396f4867f89179f5
Samuel Ievins <sievins8012@gmail.com>
2020-01-03 21:17:32 +0000

    Add avatar

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import './app.css';
+import Avatar from './avatar'
+import { useStyles } from './hooks'
+
+const styles = {
+  app: {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: '#f1f2f4',
+  }
+}
 
 function App() {
+  const classes = useStyles(styles)
+
   return (
-    <div className="app">
-      <header>
-        Alice Ievins
-      </header>
+    <div className={classes.app}>
+      <Avatar />
     </div>
   )
 }
---
+++ src/avatar/avatar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import MuiAvatar from '@material-ui/core/Avatar'
+import src from './avatar.png'
+import { useStyles } from '../hooks'
+
+const stylesDelegate = (theme) => ({
+  avatar: ({ isSmallScreen }) => ({
+    width: isSmallScreen ? theme.spacing(12) : theme.spacing(25),
+    height: isSmallScreen ? theme.spacing(12) : theme.spacing(25),
+    marginTop: isSmallScreen ? theme.spacing(2) : theme.spacing(4),
+  }),
+})
+
+export default function Avatar() {
+  const classes = useStyles(stylesDelegate)
+
+  return (
+    <MuiAvatar alt="Alice" src={src} className={classes.avatar} />
+  )
+}
