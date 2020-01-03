dee33071fb7f41feb627eb2811f1f88c9ef3d45f
Samuel Ievins <sievins8012@gmail.com>
2020-01-03 21:20:18 +0000

    Use a light theme

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import { useStyles } from './hooks'
 
 const styles = {
   app: {
-    backgroundColor: '#282c34',
+    backgroundColor: '#e2e4e9',
     minHeight: '100vh',
     display: 'flex',
     justifyContent: 'center',
     fontSize: 'calc(10px + 2vmin)',
-    color: '#f1f2f4',
+    color: '#282c34',
   }
 }
 
