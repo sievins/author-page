293f7ab2edb3a127b673f9cd0434c2607f6e3b02
Samuel Ievins <sievins8012@gmail.com>
2020-01-03 20:32:30 +0000

    Remove initial elements from create-react-app

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        Alice Ievins
+      </header>
+    </div>
+  )
+}
+
+export default App
