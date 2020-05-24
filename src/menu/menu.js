02886dd1d49c8af4dc57fdfe20a9c8738b344114
Samuel Ievins <sievins8012@gmail.com>
2020-05-24 21:26:03 +0100

    Newsletter subscription in menu

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -4,18 +4,42 @@ import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import Button from '@material-ui/core/Button'
+
+const showNewsletterSubscription = () => {
+  document.cookie = 'MCPopupClosed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;'
+  document.cookie = 'MCPopupSubscribed=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;'
+  window.dojoRequire(
+    ['mojo/signup-forms/Loader'],
+    L => {
+      L.start({
+        baseUrl: 'mc.us3.list-manage.com',
+        uuid: 'e9b41bd819600a267a1d0397f',
+        lid: 'c436762b5b',
+        uniqueMethods: true,
+      })
+    },
+  )
+}
 
 const useStyles = makeStyles(theme => ({
   bar: {
     backgroundColor: 'white',
+    height: 48,
+  },
+  home: {
+    display: 'none',
   },
   tab: {
     width: theme.spacing(40),
     maxWidth: theme.spacing(40),
     fontFamily: 'belepotan-italic',
   },
-  home: {
-    display: 'none',
+  button: {
+    fontFamily: 'belepotan-italic',
+    top: -42.2,
+    marginLeft: 'auto',
+    marginRight: 5.8,
   },
 }))
 
@@ -39,6 +63,9 @@ export default function Menu({ activeTab, setActiveTab }) {
         <Tab label="Fantasy Corner" className={classes.tab} />
         <Tab label="About Alice" className={classes.tab} />
       </Tabs>
+      <Button onClick={showNewsletterSubscription} variant="outlined" color="secondary" className={classes.button}>
+        Subscribe to newsletter
+      </Button>
     </AppBar>
   )
 }
