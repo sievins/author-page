249f399106ac40e357e90756b76a53edd4ffa460
Samuel Ievins <sievins@users.noreply.github.com>
2020-05-24 19:56:30 +0100

    Use npm instead of yarn and add menu (#2)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { ThemeProvider } from '@material-ui/styles'
 import { makeStyles } from '@material-ui/core/styles'
+import theme from './theme'
+import Menu from './menu'
 import Avatar from './avatar'
 import Corners from './corners'
 
 const useStyles = makeStyles({
   app: {
-    backgroundColor: '#e2e4e9',
+    backgroundColor: '#f2f2f2',
     minHeight: '100vh',
     display: 'flex',
     flexDirection: 'column',
@@ -17,11 +20,15 @@ const useStyles = makeStyles({
 
 function App() {
   const classes = useStyles()
+  const [activeTab, setActiveTab] = useState(0)
 
   return (
     <div className={classes.app}>
-      <Avatar />
-      <Corners />
+      <ThemeProvider theme={theme}>
+        <Menu activeTab={activeTab} setActiveTab={setActiveTab} />
+        <Avatar />
+        <Corners />
+      </ThemeProvider>
     </div>
   )
 }
