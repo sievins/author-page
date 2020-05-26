46a1085c9a5ccd9279d595b53e7decbd52fe83e3
Samuel Ievins <sievins8012@gmail.com>
2020-05-26 13:11:49 +0100

    Responsive avatar

--- src/avatar/avatar.js
+++ src/avatar/avatar.js
@@ -5,13 +5,21 @@ import src from '../assets/images/avatar.png'
 import { useStyles } from '../hooks'
 
 const useMuiStyles = makeStyles((theme) => ({
-  avatar: ({ isSmallScreen }) => ({
-    alignSelf: 'center',
-    width: isSmallScreen ? theme.spacing(12) : theme.spacing(25),
-    height: isSmallScreen ? theme.spacing(12) : theme.spacing(25),
-    marginTop: isSmallScreen ? theme.spacing(2) : theme.spacing(4),
-    marginBottom: isSmallScreen ? theme.spacing(2) : theme.spacing(4),
-  }),
+  avatar: ({ isExtraSmallScreen, isSmallScreen, isMediumScreen, isLargeScreen }) => {
+    const size =
+      isLargeScreen || isMediumScreen ? 25 :
+      isSmallScreen ? 22 : 15
+
+    const margin = isExtraSmallScreen ? 2 : 4
+
+    return {
+      alignSelf: 'center',
+      width: theme.spacing(size),
+      height: theme.spacing(size),
+      marginTop: theme.spacing(margin),
+      marginBottom: theme.spacing(margin),
+    }
+  },
 }))
 
 export default function Avatar() {
