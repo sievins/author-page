29abd51b7363e994420d67450a04ed488ceac7ef
Samuel Ievins <sievins8012@gmail.com>
2020-05-30 16:53:23 +0100

    Ripple affect on corner buttons

--- src/corners/corner/corner.js
+++ src/corners/corner/corner.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Grid from '@material-ui/core/Grid'
+import ButtonBase from '@material-ui/core/ButtonBase'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
@@ -8,16 +9,24 @@ import divider from '../../assets/images/divider.png'
 import { useStyles } from '../../hooks'
 
 const useMuiStyles = makeStyles((theme) => ({
+  grid: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  button: ({ isExtraSmallScreen }) => ({
+    margin: '0 auto',
+    marginBottom: isExtraSmallScreen && theme.spacing(2),
+  }),
   paper: ({ isExtraSmallScreen, isSmallScreen, isMediumScreen, isLargeScreen }) => {
     const width =
       isLargeScreen ? 60 :
       isMediumScreen ? 48 :
-      isSmallScreen ? 30 : 28
+      isSmallScreen ? 30 :
+      isExtraSmallScreen ? 28 : 28
 
     return {
       padding: theme.spacing(2),
       width: theme.spacing(width),
-      marginBottom: isExtraSmallScreen && theme.spacing(2),
       textAlign: 'center',
       color: theme.palette.text.primary,
       margin: 'auto',
@@ -50,21 +59,23 @@ export default function Corner({ title, authorName }) {
   const classes = useStyles(useMuiStyles)
 
   return (
-    <Grid item xs={12} sm={6}>
-      <Paper
-        elevation={elevation}
-        onMouseEnter={() => setElevation(6)}
-        onMouseLeave={() => setElevation(1)}
-        className={classes.paper}
-      >
-        <Typography variant="h1" component="h1" className={classes.h1}>
-          {title}
-        </Typography>
-        <img src={divider} alt="divider" className={classes.divider} />
-        <Typography variant="h2" component="h2" className={classes.h2}>
-          {authorName}
-        </Typography>
-      </Paper>
+    <Grid item xs={12} sm={6} className={classes.grid}>
+      <ButtonBase className={classes.button}>
+        <Paper
+          elevation={elevation}
+          onMouseEnter={() => setElevation(6)}
+          onMouseLeave={() => setElevation(1)}
+          className={classes.paper}
+        >
+          <Typography variant="h1" component="h1" className={classes.h1}>
+            {title}
+          </Typography>
+          <img src={divider} alt="divider" className={classes.divider} />
+          <Typography variant="h2" component="h2" className={classes.h2}>
+            {authorName}
+          </Typography>
+        </Paper>
+      </ButtonBase>
     </Grid>
   )
 }
