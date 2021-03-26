97e7655b53d96cc501dc79ee45e4ac3a298277c3
Samuel Ievins <sievins8012@gmail.com>
2021-03-26 21:39:12 +0000

    Add fb meta data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import './assets/images/fb-preview-logo.jpg'
 import Providers from './providers'
 import * as serviceWorker from './serviceWorker'
 
